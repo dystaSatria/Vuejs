8972ac55fb9ba4df96befdab3cf3b6b37801480d
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-11-09 22:53:03 +0300

    Add files via upload

---
+++ click-counter/app.js
@@ -0,0 +1,32 @@
+// const app = Vue.createApp({
+
+// })
+// .component('click-counter', {
+//   template: '#click-counter-template',
+//   data(){
+//     return {
+//       count: 0
+//     }
+//   }
+// })
+// .mount('#app')
+
+Vue.createApp({
+
+})
+.component( 'clicks-counter', {
+template:  '<button @click="count++">{{count}}</button>' ,
+
+// 2. turu template olusturma
+// template:  '#click-counter-template' ,
+
+
+data(){
+
+  return {
+    count :0
+  }
+}
+
+})
+.mount('#app')
